4bd4798a0b5685873f06c895d358f01685d1b162
HeySeous <jvazquez73@alumnos.uaq.mx>
2023-09-11 17:44:49 -0600

    rama tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='bg-[#2F4858] h-screen w-screen flex items-center justify-center'>
+        <div className='bg-[#25639D] h-[90%] w-[100vh] flex flex-col items-center rounded-2xl shadow-lg'>
+          <h1 className='text-5xl text-blue-400'>Hola Mundo</h1>
+
+          <button className='rounded-full bg-slate-400 hover:bg-sky-700 shadow-lg' onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+
+          <div className='bg-[#5E78C1] h-[10px] width-[20px]'>
+
+          </div>
+        </div>
+
+      </main>
     </>
   )
 }
