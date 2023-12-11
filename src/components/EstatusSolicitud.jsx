6a68e8b34b3c92e7128a42c964798b98c54fbeb6
HeySeous <jvazquez73@alumnos.uaq.mx>
2023-12-11 01:10:15 -0600

    estatus hecho

--- src/components/EstatusSolicitud.jsx
+++ src/components/EstatusSolicitud.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 function EstatusSolicitud() {
+    //Función de fetch para obtener el estatus de la solicitud
   return (
-    <div>Estatus Solicitud</div>
+    <div  className='w-screen flex flex-col justify-center'>
+        <p className='text-center text-6xl'>Estatus Solicitud</p>
+        <p className='text-center text-9xl'>Aprobada</p>
+        </div>
   )
 }
 
