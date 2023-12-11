5a78f0a777c828c57a2b8828795ab22fb1553616
HeySeous <jvazquez73@alumnos.uaq.mx>
2023-12-10 23:53:08 -0600

    Archivos front terminado

--- src/components/Archivos.jsx
+++ src/components/Archivos.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
 
 function Archivos() {
+  const subeArchivos = (nombreDocumento) => {
+    
+    console.log(`Subiendo archivo:${nombreDocumento}`);
+  };
+
   return (
-    <div>Archivos</div>
-  )
+    <div className='w-screen flex flex-col justify-center'>
+      <p className='text-center text-6xl'>Archivos</p>
+      <table className='w-3/4 mx-auto text-center'>
+        <thead>
+          <tr className='border-b-2 border-[#767676]'>
+            <th className='w-1/2 border-r-2 border-[#767676]'>Documento</th>
+            <th className='w-1/2'>Envio</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className='w-1/2'>Documento1</td>
+            <td>
+              <input
+                type='file'
+                accept='.pdf'  
+                onChange={(e) =>  subeArchivos('Documento1', e.target.files)}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className='w-1/2'>Documento2</td>
+            <td>
+              <input
+                type='file'
+                accept='.pdf'
+                onChange={(e) =>  subeArchivos('Documento2', e.target.files)}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className='w-1/2'>Documento3</td>
+            <td>
+              <input
+                type='file'
+                accept='.pdf'
+                onChange={(e) =>  subeArchivos('Documento3', e.target.files)}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Archivos
+export default Archivos;
